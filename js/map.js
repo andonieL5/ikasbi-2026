5efca0d8d4bf7ac3b5aa1d3f93abb55cfd627a6a
andonieL5 <andonietxeberrial5@gmail.com>
2026-09-10 18:52:39 +0200

    Initialize SVG map container with D3.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,14 @@
-console.log("Sistema de mapa cargado correctamente.");
+const mapContainer = document.getElementById("map");
+
+const width = mapContainer.clientWidth;
+const height = mapContainer.clientHeight;
+
+const svg = d3
+    .select("#map")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("preserveAspectRatio", "xMidYMid meet");
+
+console.log("Mapa SVG creado correctamente.");
